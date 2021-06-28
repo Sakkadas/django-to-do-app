cf1bdccefb51fbfe5591ffc9a50e11b66f9a7d58
IlyaNesterov <Eildos95@gmail.com>
2021-06-29 00:17:16 +0300

    Create class-based DetailTask view

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import TaskList
+from .views import TaskList, DetailTask
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', DetailTask.as_view(), name='task'),
 ]
--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 from .models import Task
 
@@ -9,6 +10,12 @@ class TaskList(ListView):
     model = Task
     template_name = 'tasks/main.html'
     context_object_name = 'tasks'
+
+class DetailTask(DetailView):
+    model = Task
+    template_name = 'tasks/task.html'
+    context_object_name = 'task'
+
 
 class CreateTask():
     pass
