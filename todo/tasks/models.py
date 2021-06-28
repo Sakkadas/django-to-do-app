bcf11cbb0ff039d6cb12012e9ffd7b2c9d18e402
IlyaNesterov <Eildos95@gmail.com>
2021-06-28 19:22:53 +0300

    Add new fields to Task model

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=20)
-    done = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField(null=True, blank=True)
+    complete = models.BooleanField(default=False)
     created = models.DateTimeField(auto_now_add=True)
+    important = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.name
+        return self.title
 
+    class Meta:
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+        order_with_respect_to = 'user'
