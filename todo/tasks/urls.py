1923aded248419e9a2fdaae354935ae44fbb64fe
IlyaNesterov <Eildos95@gmail.com>
2021-06-29 17:55:01 +0300

    rename `DetailTask` --> `TaskDetail`

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import TaskList, DetailTask, TaskCreate, TaskUpdate, TaskDelete, TaskLoginView, TaskRegisterView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, TaskLoginView, TaskRegisterView
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
-    path('task/<int:pk>/', DetailTask.as_view(), name='task'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
 
     path('create-task', TaskCreate.as_view(), name='create-task'),
     path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update-task'),
